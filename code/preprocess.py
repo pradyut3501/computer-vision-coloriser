9f78fbbff6bb19069bd03a72849e78692c1838c7
John <johnchung1010@gmail.com>
2023-04-28 23:29:15 -0400

    updated preprocessing

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,13 +1,14 @@
 import os
 import random
-from skimage import io, img_as_ubyte
+from skimage import io, img_as_float32
 from skimage.color import rgba2rgb, rgb2gray
 from tqdm import tqdm
 import numpy as np
 import pickle
+import math
 
 DATA_DIR = "../data"
-NUM_IMAGES = 100 # Can be increased upto 99,000
+
 
 def get_unique_ids(part):
     id_folders = os.listdir(DATA_DIR + "/raw/" + part)
@@ -24,72 +25,104 @@ def get_unique_ids(part):
                                     id + "/" + chosen_face)
             # Convert image from RGBA to RGB and grayscale
             rgb_face = rgba2rgb(chosen_face)
-            rgb_face = img_as_ubyte(rgb_face)
+            rgb_face = img_as_float32(rgb_face)
             gray_face = rgb2gray(rgb_face)
-            gray_face = img_as_ubyte(gray_face)
+            gray_face = img_as_float32(gray_face)
+            gray_face = np.expand_dims(gray_face, axis=-1)
 
             rgb_imgs.append(rgb_face)
             gray_imgs.append(gray_face)
 
     print(f"Identities loaded: {len(rgb_imgs)}")
-    return rgb_imgs, gray_imgs
+    return np.array(rgb_imgs), np.array(gray_imgs)
 
 
 def preprocess():
     parts = ["part1", "part2", "part3"]
-    all_rgb_imgs = []
-    all_gray_imgs = []
 
     for i, part in enumerate(parts):
         print(f"Preprocessing part {i + 1}")
-        processed_rgb, processed_gray = get_unique_ids(part)
-        all_rgb_imgs += processed_rgb
-        all_gray_imgs += processed_gray
+        rgb_imgs, gray_imgs = get_unique_ids(part)
 
-    return np.array(all_rgb_imgs), np.array(all_gray_imgs)
+        # Train/validation/test split: 80-10-10
+        idxs = np.random.permutation(len(rgb_imgs))
+        train_i = math.floor(0.8 * len(rgb_imgs))
+        val_i = math.floor(0.9 * len(rgb_imgs))
+
+        train_idxs = idxs[:train_i]
+        val_idxs = idxs[train_i:val_i]
+        test_idxs = idxs[val_i:]
+
+        # Store in file for train images
+        train_rgb_imgs = rgb_imgs[train_idxs]
+        train_gray_imgs = gray_imgs[train_idxs]
+        print("Storing in file for train RGB images")
+        store_data("train_rgb", train_rgb_imgs)
+        print("Storing in file for train gray images")
+        store_data("train_gray", train_gray_imgs)
+        # Store in file for validation images
+        val_rgb_imgs = rgb_imgs[val_idxs]
+        val_gray_imgs = gray_imgs[val_idxs]
+        print("Storing in file for validation RGB images")
+        store_data("validation_rgb", val_rgb_imgs)
+        print("Storing in file for validation gray images")
+        store_data("validation_gray", val_gray_imgs)
+        # Store in file for test images
+        test_rgb_imgs = rgb_imgs[test_idxs]
+        test_gray_imgs = gray_imgs[test_idxs]
+        print("Storing in file for test RGB images")
+        store_data("test_rgb", test_rgb_imgs)
+        print("Storing in file for test gray images")
+        store_data("test_gray", test_gray_imgs)
+
+
+def store_data(file_name, imgs):
+    file = open(DATA_DIR + "/pickled/" + file_name, "ab")
+    pickle.dump(imgs, file)
+    file.close()
+
+
+def load_data(file_name):
+    imgs = []
+    file = open(DATA_DIR + "/pickled/" + file_name, "rb")
+    while True:
+        try:
+            imgs.append(pickle.load(file))
+        except EOFError:
+            file.close()
+            return np.concatenate(imgs)
 
 
 class Datasets():
-    """ Class for containing the training and test sets as well as
+    """ Class for containing the training, validation, and test sets as well as
     other useful data-related information. Contains the functions
     for preprocessing.
     """
 
-    def __init__(self):
-        self.color_imgs, self.gray_imgs = self.load_data()
+    def __init__(self, is_stored=True):
+        if not is_stored:
+            preprocess()
 
-        random_order = np.arange(NUM_IMAGES)
-        np.random.shuffle(random_order)
+        # Load train images
+        self.train_rgb_imgs = load_data("train_rgb")
+        self.train_gray_imgs = load_data("train_gray")
+        # Load validation images
+        self.val_rgb_imgs = load_data("validation_rgb")
+        self.val_gray_imgs = load_data("validation_gray")
+        # Load test images
+        self.test_rgb_imgs = load_data("test_rgb")
+        self.test_gray_imgs = load_data("test_gray")
 
-        # Split 80: 10: 10
-        train = (int) (0.8 * NUM_IMAGES)
-        val = (int) (0.9 * NUM_IMAGES)
-        
-        self.train_gray = self.gray_imgs[random_order[:train]]
-        self.train_color = self.color_imgs[random_order[:train]]
 
-        self.validation_gray = self.gray_imgs[random_order[train:val]]
-        self.validation_color = self.color_imgs[random_order[train:val]]
+def main():
+    data = Datasets()
+    print(f"Train RGB: {data.train_rgb_imgs.shape}")
+    print(f"Train gray: {data.train_gray_imgs.shape}")
+    print(f"Val RGB: {data.val_rgb_imgs.shape}")
+    print(f"Val gray: {data.val_gray_imgs.shape}")
+    print(f"Test RGB: {data.test_rgb_imgs.shape}")
+    print(f"Test gray: {data.test_gray_imgs.shape}")
 
-        self.test_gray = self.gray_imgs[random_order[val:]]
-        self.test_color = self.color_imgs[random_order[val:]]
 
-    def store_data(self):
-        all_rgb_imgs, all_gray_imgs = preprocess()
-        file = open(DATA_DIR + "/pickled_data", "ab")
-        pickle.dump(all_rgb_imgs, file)
-        pickle.dump(all_gray_imgs, file)
-        file.close()
-
-    def load_data(self):
-        file = open(DATA_DIR + "/pickled_data", "rb")
-        all_rgb_imgs = pickle.load(file)
-        all_rgb_imgs = all_rgb_imgs[:NUM_IMAGES]
-        all_rgb_imgs = all_rgb_imgs.astype(float)
-
-        all_gray_imgs = pickle.load(file)
-        all_gray_imgs = all_gray_imgs[:NUM_IMAGES]
-        all_gray_imgs = np.expand_dims(all_gray_imgs, axis=-1)
-        all_gray_imgs = all_gray_imgs.astype(float)
-        file.close()
-        return all_rgb_imgs, all_gray_imgs
+if __name__ == '__main__':
+    main()
