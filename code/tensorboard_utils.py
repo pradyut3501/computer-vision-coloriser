bd421a27dcde81adff3880f2438fd8110fa2b2ea
John <johnchung1010@gmail.com>
2023-05-08 14:52:45 -0400

    fixed save

--- code/tensorboard_utils.py
+++ code/tensorboard_utils.py
@@ -216,14 +216,14 @@ class CustomModelSaver(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         """ At epoch end, weights are saved to checkpoint directory. """
 
-        min_acc_file, max_acc_file, max_acc, num_weights = \
+        min_acc_file, max_acc_file, min_acc, num_weights = \
             self.scan_weight_files()
 
         cur_acc = logs["mean_squared_error"]
 
         # Only save weights if test accuracy exceeds the previous best
         # weight file
-        if cur_acc < max_acc:
+        if cur_acc < min_acc:
             save_name = "weights.e{0:03d}-acc{1:.4f}.h5".format(
                 epoch, cur_acc)
 
@@ -245,7 +245,7 @@ class CustomModelSaver(tf.keras.callbacks.Callback):
             # minimum weight
             if self.max_num_weights > 0 and \
                     num_weights + 1 > self.max_num_weights:
-                os.remove(self.checkpoint_dir + os.sep + min_acc_file)
+                os.remove(self.checkpoint_dir + os.sep + max_acc_file)
         else:
             print(("\nEpoch {0:03d} TEST accuracy ({1:.4f}) DID NOT EXCEED "
                    "previous maximum TEST accuracy.\nNo checkpoint was "
@@ -255,10 +255,8 @@ class CustomModelSaver(tf.keras.callbacks.Callback):
         """ Scans checkpoint directory to find current minimum and maximum
         accuracy weights files as well as the number of weights. """
 
-        # min_acc = float('inf')
-        # max_acc = 0
-        min_acc = 0
-        max_acc = float('inf')
+        min_acc = float('inf')
+        max_acc = 0
         min_acc_file = ""
         max_acc_file = ""
         num_weights = 0
@@ -277,4 +275,32 @@ class CustomModelSaver(tf.keras.callbacks.Callback):
                     min_acc = file_acc
                     min_acc_file = weight_file
 
-        return min_acc_file, max_acc_file, max_acc, num_weights
+        return min_acc_file, max_acc_file, min_acc, num_weights
+
+    # def scan_weight_files(self):
+    #     """ Scans checkpoint directory to find current minimum and maximum
+    #     accuracy weights files as well as the number of weights. """
+
+    #     # min_acc = float('inf')
+    #     # max_acc = 0
+    #     min_acc = 0
+    #     max_acc = float('inf')
+    #     min_acc_file = ""
+    #     max_acc_file = ""
+    #     num_weights = 0
+
+    #     files = os.listdir(self.checkpoint_dir)
+
+    #     for weight_file in files:
+    #         if weight_file.endswith(".h5"):
+    #             num_weights += 1
+    #             file_acc = float(re.findall(
+    #                 r"[+-]?\d+\.\d+", weight_file.split("acc")[-1])[0])
+    #             if file_acc > max_acc:
+    #                 max_acc = file_acc
+    #                 max_acc_file = weight_file
+    #             if file_acc < min_acc:
+    #                 min_acc = file_acc
+    #                 min_acc_file = weight_file
+
+    #     return min_acc_file, max_acc_file, max_acc, num_weights
